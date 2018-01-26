411fa47ee2e672879fedbf4b149d3782407319c2
Sean Oughton <seanoughton@gmail.com>
2018-01-26 18:48:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,4 @@
 def reverse_each_word(string)
   # returns that same sentence with each word reversed in place.
+  #First solve it using .each Then write the same method using .collect to see the difference.
 end
