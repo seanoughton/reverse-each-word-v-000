628f328d3ba12ede8dca5473660204746d6dc0d7
Sean Oughton <seanoughton@gmail.com>
2018-01-26 19:07:23 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,1 @@
-def reverse_each_word(string)
-  original_array = string.split(" ")
-  return_array = []
-  original_array.each do|string|
-    return_array << string.reverse
-  end
-  return_array.join(" ")
-end
+
